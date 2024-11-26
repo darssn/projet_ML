f59cb20ab65c86b4f7a82ddf6769c1c57f71f483
Darcy NGUYEN <nguyendarcy@hotmail.fr>
2024-11-26 12:48:28 +0100

    ajout evaluation et ajout algorithme Linear regression

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
 import streamlit as st
 import pandas as pd
 import altair as alt
+import numpy as np
 from sklearn.ensemble import RandomForestClassifier, RandomForestRegressor
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report, accuracy_score
 from sklearn.metrics import precision_recall_fscore_support as score
 from sklearn.metrics import mean_squared_error, r2_score
+from sklearn.linear_model import LinearRegression
+from sklearn import metrics
 
 st.set_page_config(
     page_title="Projet ML",
@@ -77,94 +80,154 @@ with tabs_2:
                     
                             
 
-
+ 
 with tabs_3:
     st.header("Modélisation")
+    
+    metrics_bool = False
+    model_choice = None
+    eval = True
+    
 
     target = st.selectbox("Choisissez une colonne cible", options=data.columns)
-    y = data[target]
+  
     
+    y = data[target]
     type_data = data[target].dtype == 'object' or data[target].dtype == 'string'
 
-    if type_data:
-         X = data.drop(columns=[target])
+
+    model_choice = st.selectbox("Choisissez un algorithme", ["Random Forest", "Linear regression"])
+    
+    
+    if model_choice == "Linear regression" and type_data :
+        st.error("La colonne selectionné n'est pas numerique et ne peut pas etre entrainé")
+        eval = False
     else:
-        data = pd.get_dummies(data)
-        X = data.drop(columns=[target])
+        eval = True
+    
 
-    # model_choice = st.selectbox("Choisissez un algorithme", ["Random Forest", "Linear regression"])
-
-    test_size = st.slider("Taille de l'ensemble de test (%)", 10, 50, 20) / 100
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size)
-
-    st.write("Taille de l'ensemble d'entraînement :", len(X_train))
-    st.write("Taille de l'ensemble de test :", len(X_test))
-
-    st.write("Distribution des classes dans y_train :", y_train.value_counts())
-    st.write("Distribution des classes dans y_test :", y_test.value_counts())
-
-
-    if st.button("Entrainer le modèle"):
-     
-        
+    if eval : 
         if type_data:
-            model = RandomForestClassifier(n_estimators=100, max_depth=60)
+            X = data.drop(columns=[target])
         else:
-            model = RandomForestRegressor(n_estimators=100, max_depth=60,oob_score=True)
+            data = pd.get_dummies(data)
+            X = data.drop(columns=[target])
+
+    
+        test_size = st.slider("Taille de l'ensemble de test (%)", 10, 50, 20) / 100
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size)
+
+        st.write("Taille de l'ensemble d'entraînement :", len(X_train))
+        st.write("Taille de l'ensemble de test :", len(X_test))
+
+        st.write("Distribution des classes dans y_train :", y_train.value_counts())
+        st.write("Distribution des classes dans y_test :", y_test.value_counts())
+
         
 
-        model = model.fit(X_train, y_train)
-        st.success("Modèle entraîné avec succès")
-
-        result = model.predict(X_test)
-
-        X_test["guess_target"] = result
-        X_test["target"] = y_test
-        st.write(X_test)
-
-        if type_data :
-            # Calcul des métriques
-            precision, recall, fscore, _ = score(y_test, result, average='weighted')
-            accuracy = accuracy_score(y_test, result)
-
-            # Affichage des résultats
-            st.write("**Métriques du modèle :**")
-            st.write(f"- Precision : {round(precision, 3)}")
-            st.write(f"- Recall : {round(recall, 3)}")
-            st.write(f"- F1-score : {round(fscore, 3)}")
-            st.write(f"- Accuracy : {round(accuracy, 3)}")
-        else :          
-           # Access the OOB Score
-            oob_score = model.oob_score_
-            st.write(f'OOB Score: {oob_score:.4f}')
+        if st.button("Entrainer le modèle"):
             
-            oob_error = 1 - oob_score
-            st.write(f'OOB error: {oob_error:.4f}')
-            
-            
+            match model_choice:
+                
+                case "Random Forest":
+        
+                    if type_data:
+                        model = RandomForestClassifier(n_estimators=100, max_depth=60)
+                    else:
+                        model = RandomForestRegressor(n_estimators=100, max_depth=60,oob_score=True)
+                    
+                    model = model.fit(X_train, y_train)
+                    st.success("Modèle entraîné avec succès")
 
-            # Evaluating the model
-            mse = mean_squared_error(y_test, result)
-            st.write(f'Mean Squared Error: {mse:.4f}')
+                    result = model.predict(X_test)
 
-            r2 = r2_score(y_test, result)
-            st.write(f'R-squared: {r2:.4f}')
-    
-    
+                    X_test["guess_target"] = result
+                    X_test["target"] = y_test
+                    st.write(X_test)
+
+                    if type_data :
+                        # Calcul des métriques
+                        precision, recall, fscore, _ = score(y_test, result, average='weighted')
+                        accuracy = accuracy_score(y_test, result)
+                        
+                        metrics_bool = True
+
+                        # Affichage des résultats
+                        st.write("**Métriques du modèle :**")
+                        st.write(f"- Precision : {round(precision, 3)}")
+                        st.write(f"- Recall : {round(recall, 3)}")
+                        st.write(f"- F1-score : {round(fscore, 3)}")
+                        st.write(f"- Accuracy : {round(accuracy, 3)}")
+                    else :          
+                    # Access the OOB Score
+                        oob_score = model.oob_score_
+                        st.write(f'OOB Score: {oob_score:.4f}')
+                        
+                        oob_error = 1 - oob_score
+                        st.write(f'OOB error: {oob_error:.4f}')
+                        
+                        # Evaluating the model
+                        mse = mean_squared_error(y_test, result)
+                        st.write(f'Mean Squared Error: {mse:.4f}')
+
+                        r2 = r2_score(y_test, result)
+                        st.write(f'R-squared: {r2:.4f}')
+                        
+                        metrics_bool = True
+            
+                case "Linear regression":
+                                                               
+                    st.write("Linear regression")
+                    
+                    lm = LinearRegression()  
+                    
+                    lm.fit(X_train,y_train)
+                    
+                    st.write(lm.coef_)
+                    
+                    prediction = lm.predict(X_test)
+                    
+                    mae = metrics.mean_absolute_error(y_test,prediction)            
+                    mse = metrics.mean_squared_error(y_test,prediction)
+                    rmse = np.sqrt(metrics.mean_squared_error(y_test, prediction))
+                   
+                    metrics_bool = True
+                    
+                    st.write(f"- MAE : {mae}")
+                    st.write(f"- MSE : {mse}")
+                    st.write(f"- RMSE : {rmse}")
+                    
+                    
 with tabs_4:
-    pass
-    # st.header("Évaluation")
-
-    # if "model" in st.session_state:
-    #     model = st.session_state["model"]
+    st.header("Évaluation")
     
-    # if st.button("Évaluer le modèle"):
-    #     y_pred = model.predict(X_test)
-    #     accuracy = accuracy_score(y_test, y_pred)
-    #     st.write(f"### Précision : {accuracy:.2f}")
-    #     st.text("Rapport de classification")
-    #     st.text(classification_report(y_test, y_pred))
-    
+    if metrics_bool :
 
-    # else:
-    #     st.warning("Aucun modèle n'a été entraîné. Veuillez entraîner un modèle avant d'évaluer.")
+        match model_choice:
+            
+            case "Random Forest":
+     
+                if type_data :
+                    # Affichage des résultat
+                    st.write("**Métriques du modèle :**")
+                    st.write(f"- Precision : {round(precision, 3)}")
+                    st.write(f"- Recall : {round(recall, 3)}")
+                    st.write(f"- F1-score : {round(fscore, 3)}")
+                    st.write(f"- Accuracy : {round(accuracy, 3)}")
+                else :          
+                    # Access the OOB Score
+                    st.write(f'OOB Score: {oob_score:.4f}')
+                    st.write(f'OOB error: {oob_error:.4f}')   
+                                 
+                    # Evaluating the model
+                    st.write(f'Mean Squared Error: {mse:.4f}')
+                    st.write(f'R-squared: {r2:.4f}')
+                    
+            case "Linear regression":
+                
+                    st.write(f"- MAE : {mae}")
+                    st.write(f"- MSE : {mse}")
+                    st.write(f"- RMSE : {rmse}")
+
+    else:
+        st.warning("Aucun modèle n'a été entraîné. Veuillez entraîner un modèle avant d'évaluer.")
